7ae5a3fbb0e73b03213e23805fe11f904e74f73e
ikewinCL <kewincoro1.5@gmail.com>
2025-05-27 21:27:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,22 +6,22 @@ import plotly.express as px
 # Cargar datos
 df = pd.read_csv("data/datos_latam_limpios.csv")
 
-# Ver columnas disponibles
+# Ver columnas disponibles (útil para depurar)
 st.write("Columnas disponibles:", df.columns.tolist())
-
 
 # Título y descripción
 st.title("📊 Dashboard Económico de América Latina")
 st.markdown("Visualización interactiva de indicadores económicos entre 2010 y 2023.")
 
 # Filtros
-paises = df['country'].unique()
-indicadores = ['PIB_USD', 'Inflacion_Porcentaje', 'Tipo_Cambio_USD', 'Crecimiento_PIB_Porcentaje']
+paises = df['Pais'].unique()
+indicadores = ['PBI', 'Tasa_Inflacion', 'Tipo_Cambio', 'Estabilidad_Macro']
 
 pais = st.selectbox("Selecciona un país", paises)
 indicador = st.selectbox("Selecciona un indicador", indicadores)
 
-df_filtrado = df[df['country'] == pais]
+df_filtrado = df[df['Pais'] == pais]
 
 fig = px.line(df_filtrado, x='Año', y=indicador, title=f"{indicador} en {pais}")
 st.plotly_chart(fig)
+
